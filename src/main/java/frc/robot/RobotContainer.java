99d9c4d237397caf33fcc369b4638241ef39e3f9
kraisbeck <kraisbeck382@gmail.com>
2025-02-15 13:48:57 -0500

    feat: rewrite elevator code
    Rewrote the Elevator code to be simpler and to better account for the PID control/FeedForward logic.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,18 +4,10 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.elevator.MoveElevatorDownCommand;
-import frc.robot.commands.elevator.MoveElevatorUpCommand;
-import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.ElevatorSubsystem;
-import swervelib.SwerveInputStream;
-
 import java.util.function.DoubleSupplier;
 
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.auto.NamedCommands;
-
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.epilogue.NotLogged;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -36,23 +28,25 @@ public class RobotContainer {
   @NotLogged
   private final CommandXboxController mDriverController = new CommandXboxController(0);
 
-  // private final DrivetrainSubsystem mDrivetrainSubsystem = new DrivetrainSubsystem();
+  // private final DrivetrainSubsystem mDrivetrainSubsystem = new
+  // DrivetrainSubsystem();
 
   private final SendableChooser<Command> mCommandChooser;
 
-  private final ElevatorSubsystem  mElevatorSubsystem = new ElevatorSubsystem();
+  private final ElevatorSubsystem mElevatorSubsystem = new ElevatorSubsystem();
 
-  // SwerveInputStream driveAngularVelocity = SwerveInputStream.of(mDrivetrainSubsystem.getSwerveDrive(),
-  //                                                               () -> mDriverController.getLeftY() * -1,
-  //                                                               () -> mDriverController.getLeftX() * -1)
-  //                                                           .withControllerRotationAxis(mDriverController::getRightX)
-  //                                                           .deadband(0.8)
-  //                                                           .scaleTranslation(0.8)
-  //                                                           .allianceRelativeControl(true);
+  // SwerveInputStream driveAngularVelocity =
+  // SwerveInputStream.of(mDrivetrainSubsystem.getSwerveDrive(),
+  // () -> mDriverController.getLeftY() * -1,
+  // () -> mDriverController.getLeftX() * -1)
+  // .withControllerRotationAxis(mDriverController::getRightX)
+  // .deadband(0.8)
+  // .scaleTranslation(0.8)
+  // .allianceRelativeControl(true);
 
-  // SwerveInputStream driveRobotOriented = driveAngularVelocity.copy().robotRelative(true)
-  //                                                            .allianceRelativeControl(false);
-                                                            
+  // SwerveInputStream driveRobotOriented =
+  // driveAngularVelocity.copy().robotRelative(true)
+  // .allianceRelativeControl(false);
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -67,18 +61,19 @@ public class RobotContainer {
 
   private void configureBindings() {
 
-    // var teleopDriveWithAngularVelocity = SwerveInputStream.of(mDrivetrainSubsystem.getSwerveDrive(),
-    //     () -> mDriverController.getLeftX() * -1,
-    //     () -> mDriverController.getLeftY() * -1)
-    //     .withControllerRotationAxis(mDriverController::getRightX)
-    //     .deadband(0.5)
-    //     .scaleRotation(0.8)
-    //     .robotRelative(true);
+    // var teleopDriveWithAngularVelocity =
+    // SwerveInputStream.of(mDrivetrainSubsystem.getSwerveDrive(),
+    // () -> mDriverController.getLeftX() * -1,
+    // () -> mDriverController.getLeftY() * -1)
+    // .withControllerRotationAxis(mDriverController::getRightX)
+    // .deadband(0.5)
+    // .scaleRotation(0.8)
+    // .robotRelative(true);
 
     // mDrivetrainSubsystem.setDefaultCommand(mDrivetrainSubsystem.driveFieldOriented(driveRobotOriented));
 
-    mDriverController.y().whileTrue(new MoveElevatorUpCommand(mElevatorSubsystem));
-    mDriverController.a().whileTrue(new MoveElevatorDownCommand(mElevatorSubsystem));
+    mDriverController.y().whileTrue(mElevatorSubsystem.directCommand(() -> 0.25));
+    mDriverController.a().whileTrue(mElevatorSubsystem.directCommand(() -> -0.25));
   }
 
   /**
