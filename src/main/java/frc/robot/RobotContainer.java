57d5b40a57f2d2496ce13b9b22be0dd86db86874
Sindhura Mente <sindhuramente@gmail.com>
2025-02-22 10:35:05 -0500

    staging

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,18 +5,12 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.elevator.MoveElevatorDownCommand;
-import frc.robot.commands.elevator.MoveElevatorUpCommand;
-import frc.robot.subsystems.ClimberSubsystem;
 import frc.robot.subsystems.DrivetrainSubsystem;
-import frc.robot.subsystems.ElevatorSubsystem;
 import swervelib.SwerveInputStream;
 
 import java.util.function.DoubleSupplier;
 
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.auto.NamedCommands;
-
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.epilogue.NotLogged;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -38,23 +32,19 @@ public class RobotContainer {
   private final CommandXboxController mDriverController = new CommandXboxController(0);
 
   private final DrivetrainSubsystem mDrivetrainSubsystem = new DrivetrainSubsystem();
-  private final ClimberSubsystem mClimberSubsystem = new ClimberSubsystem();
 
   private final SendableChooser<Command> mCommandChooser;
 
-  private final ElevatorSubsystem  mElevatorSubsystem = new ElevatorSubsystem();
-
   SwerveInputStream driveAngularVelocity = SwerveInputStream.of(mDrivetrainSubsystem.getSwerveDrive(),
-      () -> mDriverController.getLeftY() * -1,
-      () -> mDriverController.getLeftX() * -1)
-      .withControllerRotationAxis(mDriverController::getRightX)
-      .deadband(0.8)
-      .scaleTranslation(0.8)
-      .allianceRelativeControl(true);
+                                                                () -> mDriverController.getLeftY() * -1,
+                                                                () -> mDriverController.getLeftX() * -1)
+                                                            .withControllerRotationAxis(mDriverController::getRightX)
+                                                            .deadband(0.8)
+                                                            .scaleTranslation(0.8)
+                                                            .allianceRelativeControl(true);
 
   SwerveInputStream driveRobotOriented = driveAngularVelocity.copy().robotRelative(true)
                                                              .allianceRelativeControl(false);
-                                                            
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -81,9 +71,7 @@ public class RobotContainer {
     //     .scaleRotation(0.8)
     //     .robotRelative(true);
 
-    mElevatorSubsystem.setDefaultCommand(mElevatorSubsystem.moveElevatorDirectCommand(() -> 0.0));
-    mDriverController.y().whileTrue(mElevatorSubsystem.moveElevatorDirectCommand(() -> 0.25));
-    mDriverController.a().whileTrue(mElevatorSubsystem.moveElevatorDirectCommand(() -> -0.25));
+    mDrivetrainSubsystem.setDefaultCommand(mDrivetrainSubsystem.driveFieldOriented(driveRobotOriented));
   }
 
   /**
